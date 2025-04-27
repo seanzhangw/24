84b6518c43a1596104d5ce951955581b8c9bc5b8
Sean Zhang <swz9@cornell.edu>
2025-04-26 23:18:14 -0400

    integrate random number generation w/ startup animations

--- asset-gen/generate_asset.py
+++ asset-gen/generate_asset.py
@@ -1,12 +1,13 @@
 from PIL import Image
-L_ratio = 6
-W_ratio = 10
+L_ratio = 7
+W_ratio = 12
 
 def getPixels(image_path):
     img = Image.open(image_path).convert('RGB')
     width, height = img.size
     new_width = width // W_ratio
     new_height = height // L_ratio
+    print(f"Original size: {width}x{height}, New size: {new_width}x{new_height}")
 
     pixels = [[0x00 for _ in range(new_width)] for _ in range(new_height)]
 
@@ -18,7 +19,7 @@ def getPixels(image_path):
         elif r > g and r > b:
             return 0x88  # Red
         else:
-            return 0xFF  # Default is white
+            return 0x66  # Default is Blue
 
     for y in range(new_height):
         for x in range(new_width):
@@ -29,8 +30,8 @@ def getPixels(image_path):
                 pixels[y][x] = get_color_byte(r, g, b)
 
     # Array Format
-    for row in pixels:
-        print("{" + ", ".join(f"0x{val:02X}" for val in row) + "},")
+    for row in pixels[2:-3]:
+        print("{" + ", ".join(f"0x{val:02X}" for val in row[1:-1]) + "},")
 
-image_path = "/content/10_of_hearts.png"
+image_path = "./content/back.png"
 getPixels(image_path)
